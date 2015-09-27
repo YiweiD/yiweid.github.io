67dc489c2564e2499581bd1f5da108f1f80ec7c7
Yiwei <yiwei0707@yahoo.ca>
2015-09-27 00:56:33 -0700

    bootstrap

---
+++ navbartest/js/functions.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$("#movingImg,body").vegas({
+    slides: [
+        { src: "images/image1.jpg" },
+        { src: "images/image2.jpg" },
+        { src: "images/image3.jpg" },
+        { src: "images/image4.jpg" }
+    ],
+    transition: 'slideLeft2'
+});
+
+});
+
